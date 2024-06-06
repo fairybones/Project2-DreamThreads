d6be1cd1996cef33cf6e77029a8d796c65a14ab5
Caleb Warford <cjwarford98@gmail.com>
2024-06-06 14:14:31 -0700

    Fix issue in scriptjs for homepage

--- develop MVC/public/homePage.script.js
+++ develop MVC/public/homePage.script.js
@@ -3,6 +3,9 @@ const womensButton = document.getElementById('womensButton');
 const mensFlyout = document.getElementById('mensFlyout');
 const womensFlyout = document.getElementById('womensFlyout');
 
+// Hide the women's flyout initially
+womensFlyout.classList.add('hidden');
+
 mensButton.addEventListener('click', () => {
     mensFlyout.classList.remove('hidden');
     womensFlyout.classList.add('hidden');
