1da6ce7625debd7f35254251d3a0098ca9da05c9
myname <you@example.com>
2024-06-10 16:17:27 -0700

    added preview for shirts page

--- develop MVC/public/productPage.script.js
+++ develop MVC/public/productPage.script.js
@@ -1,20 +1,18 @@
+// Function for opening the modal
+function openModal(modalId) {
+    // Add the is-active class to the specified modal
+    document.getElementById('modal' + modalId).classList.add("is-active");
+}
 
-                     // Function for opening the modal 
-                     function openModal()  
-                     { 
-                      // is-active class of the modal 
-                      document.getElementById("modal1").classList.add("is-active"); 
-                     } 
-                     // Adding event listeners for closing the modal 
-                      document.querySelectorAll( 
-                      ".modal-background, .modal-close," + 
-                      ".modal-card-head .delete,.modal-card-foot .button" 
-                     ) 
-                      .forEach(($el) => { 
-                        const $modal = $el.closest(".modal"); 
-                
-                        $el.addEventListener("click", () => { 
-                          // removing is-active class from modal 
-                          $modal.classList.remove("is-active"); 
-                        }); 
-                      }); 
+// Adding event listeners for closing the modal
+document.querySelectorAll(
+    ".modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button"
+)
+.forEach(($el) => {
+    const $modal = $el.closest(".modal");
+
+    $el.addEventListener("click", () => {
+        // Remove the is-active class from the closest modal
+        $modal.classList.remove("is-active");
+    });
+});
