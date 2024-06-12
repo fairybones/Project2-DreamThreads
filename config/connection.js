eb49af9452a3b909a70531ecad770d4442f8099f
myname <you@example.com>
2024-06-11 18:55:51 -0700

    pushing

--- config/connection.js
+++ config/connection.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 const sequelize = process.env.DB_URL
     ? new Sequelize(process.env.DB_URL)
     : new Sequelize(
-        process.env.DB_DB,
-        process.env.DB_USERNAME,
+        process.env.DB_NAME,
+        process.env.DB_USER,
         process.env.DB_PASSWORD,
         {
             host: 'localhost',
