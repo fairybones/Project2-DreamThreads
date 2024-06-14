ef8b372dd8a6aa623a80ad147cb41b790a75752a
aeschalna <aeschalna@gmail.com>
2024-06-13 18:38:04 -0700

    updated file endings

---
+++ public/homepage.js
@@ -0,0 +1,21 @@
+const mensButton = document.getElementById('mensButton');
+const womensButton = document.getElementById('womensButton');
+const mensFlyout = document.getElementById('mensFlyout');
+const womensFlyout = document.getElementById('womensFlyout');
+
+// Hide the women's flyout initially
+womensFlyout.classList.add('hidden');
+
+mensButton.addEventListener('click', () => {
+    mensFlyout.classList.remove('hidden');
+    womensFlyout.classList.add('hidden');
+    mensFlyout.classList.add('active');
+    womensFlyout.classList.remove('active');
+});
+
+womensButton.addEventListener('click', () => {
+    womensFlyout.classList.remove('hidden');
+    mensFlyout.classList.add('hidden');
+    womensFlyout.classList.add('active');
+    mensFlyout.classList.remove('active');
+});
