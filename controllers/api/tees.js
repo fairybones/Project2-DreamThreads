a552c0b5a2034fc2cebfb9d0a8956edcb4834463
Caleb Warford <cjwarford98@gmail.com>
2024-06-16 01:28:50 -0700

    Finished adding images

--- controllers/api/tees.js
+++ controllers/api/tees.js
@@ -5,50 +5,50 @@ router.get('/', (req, res) => {
     // Sample product data with photo URLs
     const products = [
         {
-            image: 'https://images.pexels.com/photos/7691223/pexels-photo-7691223.jpeg?auto=compress&cs=tinysrgb&w=600',
+            image: 'https://images.unsplash.com/photo-1622445275463-afa2ab738c34?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTZ8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D',
             name: 'Product 1',
             price: '$19.99',
             id: 'masc1'
         },
         { 
-            image: 'https://images.pexels.com/photos/7691223/pexels-photo-7691223.jpeg?auto=compress&cs=tinysrgb&w=600',
+            image: 'https://images.unsplash.com/photo-1623091928870-dbe5bd43ea7d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHRzaGlydHxlbnwwfHwwfHx8MA%3D%3D',
             name: 'Product 2',
             price: '$24.99',
             id: 'masc2'
     
         },
         { 
-            image: 'https://images.pexels.com/photos/6163221/pexels-photo-6163221.jpeg?auto=compress&cs=tinysrgb&w=600',
+            image: 'https://images.unsplash.com/photo-1626543750227-7686f9bd3e96?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTV8fHRzaGlydHxlbnwwfHwwfHx8MA%3D%3D',
             name: 'Product 3',
             price: '$29.99',
             id: 'masc3'
         },
         { 
-            image: 'https://images.pexels.com/photos/6163221/pexels-photo-6163221.jpeg?auto=compress&cs=tinysrgb&w=600',
+            image: 'https://images.unsplash.com/photo-1521577352947-9bb58764b69a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzl8fHRzaGlydHxlbnwwfHwwfHx8MA%3D%3D',
             name: 'Product 4',
             price: '$39.99',
             id: 'masc4'
         },
         { 
-            image: 'https://images.pexels.com/photos/6163221/pexels-photo-6163221.jpeg?auto=compress&cs=tinysrgb&w=600',
+            image: 'https://images.unsplash.com/photo-1605580528632-0718634a48df?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fHRzaGlydHxlbnwwfHwwfHx8MA%3D%3D',
             name: 'Product 5',
             price: '$49.99',
             id: 'masc5'
         },
         { 
-            image: 'https://images.pexels.com/photos/6163221/pexels-photo-6163221.jpeg?auto=compress&cs=tinysrgb&w=600',
+            image: 'https://plus.unsplash.com/premium_photo-1671656349262-1e1d3e09735c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fHRzaGlydHxlbnwwfHwwfHx8MA%3D%3D',
             name: 'Product 6',
             price: '$59.99',
             id: 'masc6'
         },
         { 
-            image: 'https://images.pexels.com/photos/6163221/pexels-photo-6163221.jpeg?auto=compress&cs=tinysrgb&w=600',
+            image: 'https://images.unsplash.com/photo-1593359684352-cb3fd6c0ff1f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjExfHx0c2hpcnR8ZW58MHx8MHx8fDA%3D',
             name: 'Product 7',
             price: '$69.99',
             id: 'masc7'
         },
         { 
-            image: 'https://images.pexels.com/photos/6163221/pexels-photo-6163221.jpeg?auto=compress&cs=tinysrgb&w=600',
+            image: 'https://images.unsplash.com/photo-1484517586036-ed3db9e3749e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjJ8fHRzaGlydHxlbnwwfHwwfHx8MA%3D%3D',
             name: 'Product 8',
             price: '$79.99',
             id: 'masc8'
